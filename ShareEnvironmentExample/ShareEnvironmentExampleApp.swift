44c16613e11cc0f51561f34af453bf2dcb2af8cf
Artem Novichkov <novichkoff93@gmail.com>
2021-02-13 16:21:31 +0600

    Initial Commit

---
+++ ShareEnvironmentExample/ShareEnvironmentExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareEnvironmentExampleApp.swift
+//  ShareEnvironmentExample
+//
+//  Created by Artem Novichkov on 13.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShareEnvironmentExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
